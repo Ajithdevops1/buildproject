d347cb10f263f1cd2247b3009fe57673b36cc766
Ajith <ajithdocgym@gmail.com>
2024-12-28 23:41:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
 
-sudo systemctl stop httpd
-sudo yum remove httpd
-sudo rm -rf /etc/httpd /var/www/html
-sudo yum install httpd
-sudo systemctl enable httpd
-sudo systemctl start httpd
